a2c4570846e16af0aeecbada52d4bf3cdbf2393a
Elena Gontovaya <lena_tuman@mail.ru>
2022-07-13 00:26:48 +0300

    Seminar_1_task_2

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -5,21 +5,56 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine ("Задача 1. В этой программе мы сравним два целых числа и покажем какое из них больше");
+// * -------------------РЕШЕНИЕ-------------------------- *
+
+// Console.WriteLine ("Задача 2. В этой программе мы сравним два целых числа и покажем какое из них больше, а какое меньше");
+// Console.WriteLine ("Введите первое число: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите второе число: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+
+// if (numA>numB)
+// {
+//     Console.WriteLine ("Большее число: " + numA + ", меньшее число: " + numB);
+// }
+// if (numA==numB)
+// {
+// Console.WriteLine ("Вы ввели два одинаковых числа");
+// }
+// else 
+// {
+//     Console.WriteLine ("Большее число: " + numB + ", меньшее число: " + numA);
+// }
+
+
+// *--------------------------------------------------------------------------------*
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// * ---------------------------РЕШЕНИЕ------------------------------ *
+
+Console.WriteLine ("Задача 4. В этой программе мы сравним три целых числа и покажем какое из них больше");
+
 Console.WriteLine ("Введите первое число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите второе число: ");
 int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int numС = Convert.ToInt32(Console.ReadLine());
+int max = numA;
 
-if (numA>numB)
+if (max<numB)
 {
-    Console.WriteLine ("Число " + numA + " больше, чем число " + numB);
+    int max = numB;
 }
-if (numA==numB)
+if (max<numC)
 {
-Console.WriteLine ("Число " + numA + " равно " + numB);
+    int max = numС;
 }
-else 
-{
-    Console.WriteLine ("Число " + numB + " больше, чем число " + numA);
-}
+
+    Console.WriteLine ("Большее число: " + max);
